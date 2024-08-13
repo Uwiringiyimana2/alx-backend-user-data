b6822681b2983be407f79b3badf76c300d560876
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-08-12 22:05:36 -0700

    user authentication

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
+
+from user import Base, User
+
+
+class DB():
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add user to database"""
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """query users"""
+        fields, values = [], []
+        for k, v in kwargs.items():
+            if hasattr(User, k):
+                fields.append(getattr(User, k))
+                values.append(v)
+            else:
+                raise InvalidRequestError()
+        res = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if res is None:
+            raise NoResultFound
+        return res
