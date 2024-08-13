442736fa05ac18bfe3cc47523abc19ce575653a7
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-08-12 23:44:54 -0700

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Hashing password"""
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +10,20 @@ def _hash_password(password: str) -> bytes:
     password = password.encode("utf-8")
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register User"""
+        user = self._db._session.query(User).filter_by(email=email).first()
+        if user:
+            raise ValueError(f"User {email} already exists")
+        hashedpw = _hash_password(password)
+        new_user = self._db.add_user(email, hashedpw)
+        return new_user
